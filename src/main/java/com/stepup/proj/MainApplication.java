e0f893157aa5a2fe26542ed246c5ddb89c13e2ed
Dmitrii Kosachev <dkosachev@t1.ru>
2025-07-11 01:08:15 +0300

    Добавка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,6 +2,7 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("11111");
     }
 }

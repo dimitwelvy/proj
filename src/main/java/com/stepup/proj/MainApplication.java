ba3866544fc87173bfa94bb16a71a7e350b7af4f
Dmitrii Kosachev <dkosachev@t1.ru>
2025-07-11 01:02:50 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,7 +2,6 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println("11111");
     }
 }
